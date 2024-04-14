0e102c4be6e40c7439437a073852c8d4cb8794a6
Klaus <klaus.cicd@outlook.com>
2024-04-14 14:49:18 +0800

    feat: 代理配置

--- vite.config.js
+++ vite.config.js
@@ -30,5 +30,15 @@ export default defineConfig({
             // __dirname 是NodeJS的一个全局变量，它表示当前正在执行的模块所在的目录的绝对路径
             '@': resolve(__dirname, './src')
         }
+    },
+    server:{
+        proxy: {
+            // 设置已api开头的地址, 都转发至http://127.0.0.1:8000
+            '/api': {
+                target: 'http://127.0.0.1:8081',
+                // 是否改写 origin，设置为 true 之后，就会把请求 API header 中的 origin，改成跟 target 里边的域名一样了
+                changeOrigin: true
+            }
+        }
     }
 })
